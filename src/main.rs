99b377f37026b922607e49adfb822999895d0177
Kiran Kumar Roy <kkroy22@gmail.com>
2022-03-22 17:20:28 +0530

    ll

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,3 @@
-#[derive(Debug, Clone)]
-pub struct ListNode {
-    pub val: i32,
-    pub next: Option<Box<ListNode>>,
-}
-
-impl ListNode {
-    #[inline]
-    pub fn new(val: i32) -> Self {
-        ListNode { next: None, val }
-    }
-
-    pub fn to_list(vec: Vec<i32>) -> Option<Box<ListNode>> {
-        let mut new_node = None;
-        for &v in vec.iter().rev() {
-            let mut node = ListNode::new(v);
-            node.next = new_node;
-            new_node = Some(Box::new(node));
-        }
-        new_node
-    }
-}
-
-// 24 18 2 3 5 7 9 12 6
 fn main() {
     use std::io::Write;
     let s = std::io::stdin();
@@ -29,46 +5,33 @@ fn main() {
     let mut bufin = std::io::BufRead::lines(std::io::BufReader::new(s.lock()));
     let mut bufwr = std::io::BufWriter::new(w.lock());
 
-    if let Some(Ok(n)) = bufin.next() {
-        let mut n = n.parse::<usize>().unwrap();
+    if let Some(Ok(_n)) = bufin.next() {
         if let Some(Ok(ele_list)) = bufin.next() {
-            let ele_list = ele_list
+            let mut ll = std::collections::LinkedList::new();
+            ele_list
                 .trim()
                 .split_whitespace()
-                .map(|x| x.parse::<i32>().unwrap())
-                .collect::<Vec<i32>>();
-            let root = ListNode::to_list(ele_list);
-
-            if let Some(root) = root {
-                let mut dis = false;
-                let mut rev: Vec<i32> = Vec::new();
-                let mut next = root;
-
-                while n > 0 {
-                    let val = next.clone().val;
-                    if val % 2 != 0 {
-                        if !dis {
-                            (0..rev.len()).for_each(|_| {
-                                write!(bufwr, "{:?} ", rev.pop().unwrap()).ok();
-                            });
-                        }
-                        write!(bufwr, "{:?} ", val).ok();
-                        dis = true;
-                    } else {
-                        rev.push(val);
-                        dis = false;
+                .for_each(|e| ll.push_back(e.parse::<i32>().unwrap()));
+            let mut dis = false;
+            let mut rev: Vec<i32> = Vec::new();
+            for val in ll {
+                if val % 2 != 0 {
+                    if !dis {
+                        (0..rev.len()).for_each(|_| {
+                            write!(bufwr, "{:?} ", rev.pop().unwrap()).ok();
+                        });
                     }
-
-                    let current = next.clone().next;
-                    if let Some(c) = current {
-                        next = c;
-                    }
-                    n -= 1;
+                    write!(bufwr, "{:?} ", val).ok();
+                    dis = true;
+                } else {
+                    rev.push(val);
+                    dis = false;
                 }
-                (0..rev.len()).for_each(|_| {
-                    write!(bufwr, "{:?} ", rev.pop().unwrap()).ok();
-                });
             }
+
+            (0..rev.len()).for_each(|_| {
+                write!(bufwr, "{:?} ", rev.pop().unwrap()).ok();
+            });
         }
     }
 }
